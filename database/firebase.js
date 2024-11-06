f31fc12829742f0eb80550470ecac353f9e13b0c
Gwendolyn Patwardhan <52841252+gwynin@users.noreply.github.com>
2024-11-05 20:29:52 -0800

    Write translated word and audio 64 bit string to database

--- database/firebase.js
+++ database/firebase.js
@@ -9,24 +9,24 @@ initializeApp({
 
 const db = getFirestore();
 
-user = 'carrion';
-word = 'fish';
-translated = 'peixe';
+// user = 'carrion';
+// word = 'fish';
+// translated = 'peixe';
 //var sound = require("../peixe.mp3");
 
 //for images/sound firestore doesnt actually store files, so must use mp3
-async function writeData(user, word, translated) {
-  const docRef = db.collection('users').doc(user);
+async function writeCard(card, word, translated, image, sound) {
+  const docRef = db.collection('cards').doc(card);
   await docRef.set({
-    en: word,
-    pt: translated,
-    image: "image url",
-    audio: "sound url"
+    english: word,
+    portuguese: translated,
+    image: image,
+    audio: sound
   });
   console.log('Document written successfully');
 }
 
-async function readData() {
+ async function readData() {
   const snapshot = await db.collection('users').get();
   snapshot.forEach((doc) => {
     console.log(doc.id, '=>', doc.data());
@@ -34,29 +34,6 @@ async function readData() {
 }
 
 //writeData(user, word, translated).catch(console.error);
-readData();
+//readData();
 
-/*
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAQ8JW1j_GOV8ldqKCLJRQwC0RqMT3GeZM",
-  authDomain: "instalearn-51628.firebaseapp.com",
-  databaseURL: "https://instalearn-51628-default-rtdb.firebaseio.com",
-  projectId: "instalearn-51628",
-  storageBucket: "instalearn-51628.appspot.com",
-  messagingSenderId: "774690452779",
-  appId: "1:774690452779:web:7e378a292d64db14ce016e",
-  measurementId: "G-K4QPHLR83W"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-*/
+module.exports = { writeCard, readData };
