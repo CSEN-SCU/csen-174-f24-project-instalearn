804ec56528bdf7e64d09a43645b899e129468e85
Riana-S <rianasantos808@gmail.com>
2024-11-12 15:26:26 -0800

    Updated database services with functions
    Functions to show on frontend and started function for deciding between database or API

--- databaseServices.js
+++ databaseServices.js
@@ -10,6 +10,37 @@ initializeApp({
 
 const db = getFirestore();
 
+// put HTML markup into a <div> and reveal it
+function showDiv(id, html) {
+  var content = document.getElementById("_" + id)
+  if (content) content.innerHTML = html;
+  var wrapper = document.getElementById(id);
+  if (wrapper) wrapper.style.display = html.trim() ? "block" : "none";
+}
+
+//FUNCTIONS TO SHOW IMAGE, AUDIO, AND TRANSLATION IN CARD DIVS
+function showImage(imageFile){
+  var html = [];
+  var height = 120;
+  var width = Math.max(Math.round(height * item.thumbnail.width / item.thumbnail.height), 120);
+  html.push("<p class='images' style='max-width: " + width + "px'>");
+  html.push("<img src='"+ imageFile + "&h=" + height + "&w=" + width + "' height=" + height + " width=" + width + "'>");
+  return html.join("");
+}
+
+function showAudio(audioFile){
+  var html = [];
+  temp = "<audio controls src='" + audioFile + "'>";
+  html.push(temp);
+  return html.join("");
+}
+
+function showTerm(term){
+  var html = [];
+  html.push(term);
+  return html.join("");
+}
+
 export async function writeCard(card, word, translated, image, sound) {
   const docRef = db.collection('cards').doc(card);
   await docRef.set({
@@ -24,6 +55,10 @@ export async function writeCard(card, word, translated, image, sound) {
 export async function getCard(word) {
   const card = await db.collection('cards').doc(word).get();
   console.log(card.id, '=>', card.data());
+  //FUNCTION CALLS TO DISPLAY ON FRONTEND (IDK IF SHOULD PUT HERE)
+  showDiv("image", showImage(card.image));
+  showDiv("pronunciation", showAudio(card.audio));
+  showDiv("translation", showTerm(card.portuguese));
   return card.exists ? card.data() : null;
 }
 
@@ -46,3 +81,20 @@ export async function getSet(setid){
 //debugging
 //writeCardToSet("hand", "body", "userid1");
 //getSet("body");
+
+//FUNCTION TO SEARCH DB OR SEARCH API SOURCES
+function searchCard(form){
+  var searchTerm = form.value;
+  console.log(getCard(searchTerm));
+  if(getCard(searchTerm)){
+    console.log("DB WORK");
+
+    //DISPLAY TO FRONTEND
+  }
+  else{
+    //INSERT API SERVICES FUNCTION CALLS
+    console.log("API WORK");
+    //WRITE TO DB FUNCTION WITH API RESULTS
+    //DISPLAY TO FRONTEND\
+  }
+}
