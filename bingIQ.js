d805a8ca62d6f5c030a53d88685da11024fb3509
alexlucaslopes <alex.lucas.lopes@gmail.com>
2024-11-19 15:20:40 -0800

    Oauth (The beginnings of it)

--- bingIQ.js
+++ bingIQ.js
@@ -1,15 +1,11 @@
 //import http from 'http';
 import express from "express";
 import bodyParser from "body-parser";
-import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
+import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import path from "path";
-
-// Initialize Firebase Admin SDK
-
-// THIS IS THE PROBLEM LINE IN QUESTION
-import serviceAccount from "./creds.json"; // Update path to your Firebase service account key
-//sorry to yell I just wanted to make it obvious
+import { fileURLToPath } from "url";
+import serviceAccount from "./database/creds.json" assert { type: "json"}; // Update path to your Firebase service account key
 
 initializeApp({
   credential: cert(serviceAccount),
@@ -32,23 +28,14 @@ async function authenticate(req, res, next) {
   }
 }
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //import path from 'path';
 const app = express();
 var port = 8000;
 
 app.use(bodyParser.json());
-
-// Serve static files (e.g., index.html)
-app.use(express.static(path.join(__dirname, "public")));
-
-/*app.get('/', (req, res) => {
-    res.sendFile('index.html');
- });*/
-
-/*app.listen(port, function () {
-    console.log('Starting the Node.js server for this sample. Navigate to http://localhost:'+port+'/ to view the webpage.');
-});*/
-
 
 // Route to verify ID token from the client
 app.post("/verify-token", async (req, res) => {
@@ -64,13 +51,20 @@ app.post("/verify-token", async (req, res) => {
 });
 
 // Protected route
-app.get("/", authenticate, (req, res) => {
-  res.sendFile(path.join(__dirname, "protected", "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/app", authenticate, (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "app.html"));
 });
 
 app.get('/search', (req, res) => {
   res.sendFile('bingIQ.html', {root:'public'});
 });
 
+// Serve static files (e.g., index.html)
+app.use(express.static(path.join(__dirname, "public")));
+
 // Start the server
-app.listen(8000, () => console.log("Server running on http://localhost:8000"));
+app.listen(port, () => console.log("Server running on http://localhost:8000"));
